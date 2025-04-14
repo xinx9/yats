a07e7670666b9b0ef5538f60d27eeea42ae9180e
Samuel Schmitt <sam.schmitty@gmail.com>
2025-04-14 09:35:51 -0500

    reading from db works.

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	customermodel "yats/services/models"
+)
+
 func main() {
-	
+	var customer = customermodel.GetCustomerById(1)
+
+	fmt.Printf("personId: %d, name: %s, phone: %d, email: %s ", customer.CustomerId, customer.Name, customer.Phone, customer.Email)
 }
