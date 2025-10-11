76fd0c9b22e06aa3dab563febd2373755a8af959
Samuel Schmitt <sam.schmitty@gmail.com>
2025-10-11 16:22:23 -0500

    added a logo and simple navbar layout.

--- frontend/src/features/navbar/NavBar.tsx
+++ frontend/src/features/navbar/NavBar.tsx
@@ -1,31 +1,40 @@
-//import type { JSX } from "react"
+import type { JSX } from "react"
 import styles from "./NavBar.module.css"
+import logo from "/src/yatslogo.svg"
 
-export const NavBar = () => {
+export const NavBar = (): JSX.Element => {
 
     return (
-        <div className={styles.NavBar}>
-            <span>
-                <a
-                    className={styles.NavBar}
-                    href=""
-                >
-                    Home
-                </a>
-                <a
-                    className={styles.NavBar}
-                    href=""
-                >
-                    About
-                </a>
-                <a
-                    className={styles.NavBar}
-                    href=""
-                >
-                    Contributors
-                </a>
-                <button className="align-right">sign in</button>
-            </span>
+        <div>
+            <div id="logo" className={styles.Logo}>
+                <img src={logo} alt="logo" />
+            </div>
+            <ul className={styles.NavBar}>
+                <li>
+                    <a
+                        href=""
+                    >
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href=""
+                    >
+                        Services
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href=""
+                    >
+                        Contributors
+                    </a>
+                </li>
+                <li>
+                    <button className={styles.YatsButton}>sign in</button>
+                </li>
+            </ul>
         </div>
     )
 }
